efd78997f7922323b3202f3677f9f1421f5f78cc
santyago0 <santyago0515@gmail.com>
2025-06-12 22:53:47 -0500

    Finalizacion del taller09-semana10

--- taller/deporteecuador/futbolec/admin.py
+++ taller/deporteecuador/futbolec/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from futbolec.models import *
+
+# Agregar la clase EquipoFutbol para administrar desde interfaz de administración
+# Se crea una clase que hereda de ModelAdmin para el modelo EquipoFutbol
+class EquipoFutbolAdmin(admin.ModelAdmin):
+    # Listado de atributos que se mostrará por cada registro
+    # Dejamos de usar la representación (str) de la clase
+    list_display = ('nombre', 'siglas', 'username_twitter')
+    search_fields = ('nombre', 'username_twitter')
+    exclude = ("campeonatos",)
+
+admin.site.register(EquipoFutbol, EquipoFutbolAdmin)
+
+# Agregar la clase Jugador para administrar desde interfaz de administración
+# Se crea una clase que hereda de ModelAdmin para el modelo Jugador
+class JugadorAdmin(admin.ModelAdmin):
+    # Listado de atributos que se mostrará por cada registro
+    # Dejamos de usar la representación (str) de la clase
+    list_display = ('nombre', 'posicion_campo', 'numero_camiseta', 'sueldo', 'get_equipo')
+    search_fields = ('nombre', 'numero_camiseta', 'equipo__nombre')
+
+    def get_equipo(self, objt):
+        return objt.equipo.nombre
+
+admin.site.register(Jugador, JugadorAdmin)
+
+# Agregar la clase Campeonato para administrar desde interfaz de administración
+# Se crea una clase que hereda de ModelAdmin para el modelo Campeonato
+class CampeonatoAdmin(admin.ModelAdmin):
+    # Listado de atributos que se mostrará por cada registro
+    # Dejamos de usar la representación (str) de la clase
+    list_display = ('nombre_campeonato', 'nombre_auspiciante')
+    search_fields = ('nombre_campeonato',)
+    exclude = ('equipos',)
+
+admin.site.register(Campeonato, CampeonatoAdmin)
+
+# Agregar la clase CampeonatoEquipos para administrar desde interfaz de administración
+# Se crea una clase que hereda de ModelAdmin para el modelo CampeonatoEquipos
+class CampeonatoEquiposAdmin(admin.ModelAdmin):
+    # Listado de atributos que se mostrará por cada registro
+    # Dejamos de usar la representación (str) de la clase
+    list_display = ('anio', 'equipo_futbol', 'campeonato')
+    search_fields = ('equipo_futbol__nombre',)
+
+admin.site.register(CampeonatoEquipos, CampeonatoEquiposAdmin)
--- taller/deporteecuador/futbolec/models.py
+++ taller/deporteecuador/futbolec/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+class EquipoFutbol(models.Model):
+    nombre = models.CharField(max_length=30)
+    siglas = models.CharField(max_length=5)
+    username_twitter = models.CharField(max_length=30)
+
+    campeonatos = models.ManyToManyField('Campeonato', through='CampeonatoEquipos')
+
+    def __str__(self):
+        return "%s (%s) - %s" % (self.nombre,
+                self.siglas,
+                self.username_twitter)
+
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=30)
+    posicion_campo = models.CharField(max_length=30)
+    numero_camiseta = models.IntegerField()
+    sueldo = models.IntegerField()
+
+    equipo = models.ForeignKey(EquipoFutbol, related_name='jugadores',
+            on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return "%s (%d) - %s | $%d" % (self.nombre,
+                self.numero_camiseta,
+                self.posicion_campo,
+                self.sueldo)
+
+
+class Campeonato(models.Model):
+    nombre_campeonato = models.CharField(max_length=30)
+    nombre_auspiciante = models.CharField(max_length=30)
+
+    equipos = models.ManyToManyField(EquipoFutbol, through='CampeonatoEquipos')
+
+    def __str__(self):
+        return "%s - %s" % (self.nombre_campeonato, self.nombre_auspiciante)
+    
+
+class CampeonatoEquipos(models.Model):
+    equipo_futbol = models.ForeignKey(EquipoFutbol, related_name='campeonatoequipos',
+            on_delete=models.CASCADE)
+    campeonato = models.ForeignKey('Campeonato', related_name='campeonatoequipos',
+            on_delete=models.CASCADE)
+    
+    anio = models.IntegerField()
+    
+    def __str__(self):
+        return "%d | Campeonato: (%s) - Participante: %s" % (self.anio,
+                self.campeonato,
+                self.equipo_futbol.nombre)
